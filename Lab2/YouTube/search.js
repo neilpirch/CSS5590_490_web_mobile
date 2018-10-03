f9fa3f1113035e355c254146d0d08b898316f220
Viral1101 <Viral1101@users.noreply.github.com>
2018-10-03 01:16:34 -0500

    code fixes
    altered the code to use angular.js paradigms instead of jquery; fixed accessing the JSON and started the dynamic response in the HTML

--- Lab2/YouTube/search.js
+++ Lab2/YouTube/search.js
@@ -4,19 +4,23 @@ var apiKey = "AIzaSyBhAcbOBlU7lepVO-jyqtq7g1j9lRhT-_c";
 angular.module('searchApp', [])
 
     .controller('searchController', function ($scope, $http) {
+
+        $scope.query = "";
+        $scope.searchResult = "";
+
         $scope.search = function () {
             console.log("search clicked");
-            var q = $('#query').val();
+            let q = $scope.query;
             if (q != null && q !== "") {
-                var handler = $http.get(url + "search?" +
+                let handler = $http.get(url + "search?" +
                     "part=" + "snippet" +
-                    "&q=" + q +
+                    "&q=" + $scope.query +
                     "&type=" + "" +
                     "&key=" + apiKey);
                 handler.success(function (response) {
                     console.log(response);
-                    var str = JSON.stringify(response.result);
-                    $('#search-container').html('<pre>' + str + '</pre>');
+                    $scope.searchResult = response.items;
+                    //$('#search-container').html('<pre>' + str + '</pre>');
                 });
                 handler.error(function (response) {
                     alert("There was some error processing your request.")
